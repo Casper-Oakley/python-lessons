849f9d42c8726ba5a979bf1d2ba727b996efdf62
Casper Oakley <casper_oakley@hotmail.com>
2016-11-01 14:16:42 +0000

    Created all resources including slides for lesson 1 of python lessons

---
+++ lesson1/src/helloworld.py
@@ -0,0 +1,13 @@
+print("Welcome to our number adder!!")
+print("Please input a number.")
+
+input1 = int(input())
+
+print("Please input another number.")
+
+input2 = int(input())
+
+output = input1 + input2
+
+print(str(input1) + " + " + str(input2) + " = " + str(output))
+
